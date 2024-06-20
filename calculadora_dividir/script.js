97875dc77b2707747e27e203a42b952985b05d20
GuiFz <164812474+GuiFz@users.noreply.github.com>
2024-06-20 16:23:07 -0300

    Projeto - Calculadora Dividir
    Projeto adicionado

---
+++ calculadora_dividir/script.js
@@ -0,0 +1,32 @@
+let divisor = 0;
+let dividento = 0;
+let resultado  = 0;
+
+class DivisaoPorZero extends Error{
+    constructor(message){
+        super("Não é Possível Dividir por (0) Zero.");
+    }
+}
+class InputVazia extends Error{
+    constructor(message){
+        super("Preencha  Dividendo e Divisor usando Números")
+    }
+}
+
+function calcular(){
+    divisor = parseFloat(document.getElementById("divisor").value);
+    dividendo = parseFloat(document.getElementById("dividendo").value);
+    try{
+        if(!dividendo || !divisor ){
+            throw new InputVazia();
+        }else if(divisor == 0){
+            throw new DivisaoPorZero();
+        }
+        resultado = dividendo/divisor;
+        document.getElementById("saida").innerHTML = resultado.toPrecision(3); 
+    }catch(e){
+        alert(" "+e);
+    }finally{
+        console.log("Operação Realizada");
+    }
+}
